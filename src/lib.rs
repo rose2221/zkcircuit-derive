265168fcbc271a3231e0c540d6a254a9ce9a1296
rose2221 <rose.jethani@nethermind.io>
2025-06-19 05:03:54 +0530

    builder-api

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,10 @@ struct FieldInfo{
     is_input: bool,
 }
 
-fn get_inner_type(ty: &Type) -> &Type {
+fn get_inner_type(ty: &Type) -> Option<&Path> {
     if let Type::Path(type_path) = ty {
         if let Some(segment) = type_path.path.segments.last() {
-            if segment.ident() == "Option" {
+            if segment.ident == "Option" {
                 if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
                     if let Some(syn::GenericArgument::Type(inner_ty)) = args.args.first() {
                         if let Type::Path(inner_path) = inner_ty {
@@ -61,6 +61,48 @@ pub fn zkcircuit_derive(input: TokenStream) -> TokenStream {
             }
         }
     });
+    let builder_fields = fields.iter().map(|f|{
+        let field_name = &f.name;
+        let field_ty = &f.ty;
+        quote!{
+            #field_name: #field_ty
+        }
+    });
+    let build_setters = fields.iter().map(|f|{
+        let field_name =&f.name;
+        let inner_ty = get_inner_type(&f.ty).expect("Circuit fields must be Option<T> or T");
+
+        quote! {
+            pub fn #field_name(mut self, value: #inner_ty) -> Self {
+                self.#field_name = Some(value);
+                self
+            }
+        }
+    });
+
+    let build_checks = fields.iter().maps(|f|{
+        let field_name = &f.name;
+        let error_msg = format!("{} field is missing", field_name);
+        quote! {
+            if self.#field_name.is_none() {
+                return Err(#error_msg);
+            }
+        }
+    });
+
+    let assignments = field.iter().map(|f| {
+        let field_name = &f.name;
+        quote! {
+            #field_name : self.#field_name
+        }
+    });
+    let build_assignemnts = fields.iter().map(|f| {
+        let field_name = &f.name;
+        quote! {
+            #field_name: self.#field_name
+        }
+    });
+    let build_assignments = assignments.clone();
     let gen = quote! {
         impl #impl_generics bellman::Circuit<F> for #struct_name #ty_generics #where_clause {
             fn synthesis<CS: bellman::ConstraintSystem<F>>(
@@ -72,6 +114,33 @@ pub fn zkcircuit_derive(input: TokenStream) -> TokenStream {
                 // Return Ok if all allocations were successful
                 Ok(())
             }
+        }
+        pub struct #builder_name #impl_generics #where_clause {
+            #(#builder_fields),*
+        }
+
+        impl #impl_generics #builder_name #ty_generics #where_clause {
+            pub fn new() -> Self {
+                Self {
+                    #(#builder_fields: None),*
+                }
+            }
+
+            #(#build_setters)*
+
+            pub fn build(self) -> Result<#struct_name #ty_generics, String> {
+                #(#build_checks)*
+                Ok(#struct_name {
+                    #(#build_assignments),*
+                })
+            }
+        }
+        #(#builder_setters)*
+        pub fn build(self) -> Result<#struct_name #ty_generics, &'static str>{
+            #(#build_checks)*
+            Ok(#struct_name {
+                #(#build_assignments),*
+            })
         }
     };
     gen.into()
