6fe95db47af1370133f3fbc2c429d8dd2f26e6b9
rose2221 <rose.jethani@nethermind.io>
2025-06-23 03:04:05 +0530

    added macro

--- target/debug/build/proc-macro2-3c7cf2cb897bcc27/build_script_build-3c7cf2cb897bcc27.d
+++
@@ -1,5 +0,0 @@
-/home/rose2221/workspace/zkcircuit/zk-circuit/target/debug/build/proc-macro2-3c7cf2cb897bcc27/build_script_build-3c7cf2cb897bcc27: /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/rose2221/workspace/zkcircuit/zk-circuit/target/debug/build/proc-macro2-3c7cf2cb897bcc27/build_script_build-3c7cf2cb897bcc27.d: /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs:
--- target/debug/deps/unicode_ident-43c3b454bec3b76f.d
+++
@@ -1,8 +0,0 @@
-/home/rose2221/workspace/zkcircuit/zk-circuit/target/debug/deps/libunicode_ident-43c3b454bec3b76f.rmeta: /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/home/rose2221/workspace/zkcircuit/zk-circuit/target/debug/deps/libunicode_ident-43c3b454bec3b76f.rlib: /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/home/rose2221/workspace/zkcircuit/zk-circuit/target/debug/deps/unicode_ident-43c3b454bec3b76f.d: /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs:
-/home/rose2221/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs:
